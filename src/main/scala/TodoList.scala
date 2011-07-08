dee2242ffb7cf1277b871f82b722d7bbcd77d28d
Robert Thau <rst@ai.mit.edu>
2011-07-08 12:12:56 -0400

    Initial commit --- copy sample app source from main lib project.

---
+++ src/main/scala/TodoList.scala
@@ -0,0 +1,202 @@
+package org.positronicnet.sample.todo
+
+import android.util.Log
+import android.content.Intent
+
+import org.positronicnet.db.Database
+import org.positronicnet.db.PositronicCursor
+import org.positronicnet.db.DbQuery
+
+import org.positronicnet.util.WorkerThread
+import org.positronicnet.util.ChangeManager
+
+// Our domain model classes, such as they are:  Todo Items, Lists, etc.
+// 
+// There's no ORM here, just an AREL-style gloss for building SQL, and
+// a stylized way of building model classes that use it.  But it still
+// cuts down on the clutter.
+//
+// NB operations on these affect the database, so they happen on a
+// separate "db thread".  UI components can register as listeners for
+// changes on domain objects, and if they do, they get fresh cursors
+// with which to update themselves when things do change.  
+//
+// Note also that we're managing a "soft deletion" scheme here.
+// User-level "delete" operations just set an "is_deleted" flag on the
+// objects of the user's disaffection; they don't actually delete them
+// immediately.  If the user has second thoughts, they can then
+// "undelete" (resetting the flag) until the next batch of deletions,
+// at which point the last batch really is purged.  This tends to be
+// more effective than confirmation dialogs at helping users recover
+// from mistakes.
+
+// Start by defining the DB schema...
+
+object TodoDb 
+ extends Database( filename = "todos.sqlite3", logTag = "todo" ) 
+ with WorkerThread
+{
+  // This gets fed to a SQLiteOpenHelper, which implements the following
+  // default behavior (unless overridden, of course):
+  //
+  // "version" is the length of schemaUpdates.
+  // "onUpdate" runs all the updates from oldVersion to newVersion.
+  // "onCreate" just runs 'em all.
+
+  def schemaUpdates =
+    List(""" create table todo_lists (
+               _id integer primary key,
+               name string
+             )
+         """,
+         """ create table todo_items (
+               _id integer primary key,
+               todo_list_id integer,
+               description string,
+               is_done integer
+             )
+         """,
+         " alter table todo_lists add column is_deleted integer default 0 ",
+         " alter table todo_items add column is_deleted integer default 0 "
+        )
+  
+}
+
+//================================================================
+// "Todo item" model.
+// 
+// Mostly actually manipulated from within TodoList; with a more
+// complicated schema, it might be better to get these query fragments
+// from methods invoked on the TodoItem companion object.
+
+case class TodoItem(var id: Long, var description: String, var isDone: Boolean)
+
+object TodoItem {
+
+  def doQuery( query: DbQuery )= query.select("_id", "description", "is_done")
+
+  def fromCursor( c: PositronicCursor ) = 
+    TodoItem( c.getLong( 0 ), c.getString( 1 ), c.getBoolean( 2 ))
+
+}
+
+//================================================================
+// "Todo list" model.  
+// Includes most actual manipulation of items.
+
+case class TodoList( var id: Long, var name: String )
+ extends ChangeManager( TodoDb )
+{
+  // Setting up (and use of) prebaked query fragments.
+
+  private lazy val dbItemsAll = TodoDb("todo_items").whereEq("todo_list_id"->id)
+  private lazy val dbItems    = dbItemsAll.whereEq( "is_deleted" -> false )
+
+  // Things that UI elements (etc.) can monitor
+
+  lazy val items = cursorStream { TodoItem.doQuery( dbItems ) }
+
+  def itemsQuery( initialShowDone: Boolean ) = {
+    cursorQuery( initialShowDone ){ showDone => 
+      TodoItem.doQuery(
+        if ( showDone ) dbItems else dbItems.whereEq( "is_done" -> false )
+      )
+    }
+  }
+
+  lazy val numDoneItems = valueStream { 
+    dbItems.whereEq( "is_done" -> true ).count 
+  }
+  lazy val numDeletedItems= valueStream { 
+    dbItemsAll.whereEq( "is_deleted" -> true).count
+  }
+
+  // Changes that UI elements (etc.) can ask for
+
+  def addItem( description: String, isDone: Boolean = false ) = doChange { 
+    TodoDb( "todo_items" ).insert( 
+        "todo_list_id" -> this.id, 
+        "description"  -> description,
+        "is_done"      -> isDone )
+  }
+
+  def setItemDescription( it: TodoItem, desc: String ) = doChange { 
+    dbItems.whereEq("_id" -> it.id).update( "description" -> desc )
+  }
+
+  def setItemDone( it: TodoItem, isDone: Boolean ) = doChange { 
+    dbItems.whereEq("_id" -> it.id).update( "is_done" -> isDone )
+  }
+
+  def deleteWhereDone = doChange {
+    dbItemsAll.whereEq( "is_deleted" -> true ).delete // purge the last batch
+    dbItems.whereEq( "is_done" -> true ).update( "is_deleted" -> true )
+  }
+
+  def undeleteItems = doChange { dbItemsAll.update( "is_deleted" -> false ) }
+}
+
+object TodoList {
+
+  def doQuery( query: DbQuery ) = query.select("_id", "name")
+
+  def fromCursor( c: PositronicCursor ) = 
+    TodoList( c.getLong( 0 ), c.getString( 1 ))
+
+  def create( name: String ) = TodoDb( "todo_lists" ).insert( "name" -> name )
+
+  // Communicating these through intents...
+  // Sadly, this is easier than making them serializable.
+
+  val intentIdKey = "todoListId"; val intentNameKey = "todoListName"
+
+  def intoIntent( list: TodoList, intent: Intent ) = {
+    intent.putExtra( intentIdKey,   list.id )
+    intent.putExtra( intentNameKey, list.name )
+  }
+
+  def fromIntent( intent: Intent ) = 
+    TodoList( intent.getLongExtra( intentIdKey, -1 ), 
+              intent.getStringExtra( intentNameKey ))
+}
+
+//================================================================
+// Singleton object to represent the set of all available lists.
+
+object TodoLists extends ChangeManager( TodoDb )
+{
+  private lazy val dbListsAll = TodoDb("todo_lists")
+  private lazy val dbLists = dbListsAll.whereEq("is_deleted"-> false)
+
+  // Things UI can monitor
+
+  lazy val lists = cursorStream { TodoList.doQuery( dbLists ) }
+
+  lazy val numDeletedLists= valueStream {
+    dbListsAll.whereEq("is_deleted"-> true).count
+  }
+
+  // Changes UI can request
+
+  def addList( name: String ) = doChange { TodoList.create( name ) }
+
+  def setListName( list: TodoList, newName: String ) = doChange {
+    dbLists.whereEq("_id" -> list.id).update( "name" -> newName )
+  }
+
+  def removeList( victim: TodoList ) = doChange {
+
+    // Purge all previously deleted lists...
+    for ( c <- dbListsAll.whereEq( "is_deleted" -> true ).select( "_id" )) {
+      val purgedListId = c.getLong(0)
+      TodoDb( "todo_items" ).whereEq( "todo_list_id" -> purgedListId ).delete
+      TodoDb( "todo_lists" ).whereEq( "_id" -> purgedListId ).delete
+    }
+
+    // And mark this one for the axe...
+    TodoDb("todo_lists").whereEq("_id"->victim.id).update("is_deleted" -> true)
+  }
+
+  def undelete = doChange { dbListsAll.update( "is_deleted" -> false ) }
+} 
+
---
+++ src/main/scala/TodoUI.scala
@@ -0,0 +1,296 @@
+package org.positronicnet.sample.todo
+
+import org.positronicnet.ui.IndexedSeqAdapter
+import org.positronicnet.ui.PositronicDialog
+import org.positronicnet.ui.PositronicActivity
+import org.positronicnet.ui.CursorSourceAdapter
+
+import org.positronicnet.util.ChangeNotifications
+import org.positronicnet.db.PositronicCursor
+
+import android.app.Activity
+import android.os.Bundle
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.ContextMenu
+import android.widget.TextView
+import android.widget.Toast
+import android.graphics.Paint
+import android.graphics.Canvas
+
+// Adapter to wire up TodoList changes to the UI.
+//
+// Registers with the "source" (our TodoLists singleton) to be
+// notified whenever its underlying data set is changed (or reloaded),
+// so long as the activity is running.
+
+class TodosAdapter( activity: PositronicActivity )
+ extends CursorSourceAdapter( activity, 
+                              source = TodoLists.lists,
+                              converter = TodoList.fromCursor(_),
+                              itemViewResourceId = R.layout.todos_row )
+{
+  def bindItem( view: View, list: TodoList ) =
+    view.asInstanceOf[ TextView ].setText( list.name )
+}
+
+// Activity that uses it:
+
+class TodosActivity 
+ extends PositronicActivity( layoutResourceId = R.layout.all_todos ) 
+ with ViewFinder 
+{
+  lazy val listsView = findView( TR.listsView )
+  lazy val renameDialog = new EditStringDialog( this )
+
+  onCreate {
+
+    useOptionsMenuResource( R.menu.lists_view_menu )
+    useContextMenuResource( R.menu.lists_context_menu )
+
+    // Wire listsView to the database
+
+    useAppFacility( TodoDb )            // Open DB; arrange to close on destroy
+    listsView.setAdapter( new TodosAdapter( this ))
+
+    // Listen for events on widgets
+
+    listsView.onItemClick { (view, posn, id) => viewListAt( posn ) }
+
+    findView( TR.addButton ).onClick { doAdd }
+    findView( TR.newListName ).onKey( KeyEvent.KEYCODE_ENTER ){ doAdd }
+
+    onOptionsItemSelected( R.id.undelete ) { doUndelete }
+
+    registerForContextMenu( listsView )
+
+    onContextItemSelected( R.id.rename ){ 
+      (menuInfo, view) => doRename( getContextItem( menuInfo, view ))
+    }
+    onContextItemSelected( R.id.delete ){ 
+      (menuInfo, view) => doDelete( getContextItem( menuInfo, view ))
+    }
+  }
+
+  // Determining relevant context for the ContextMenu
+
+  def getContextItem( menuInfo: ContextMenu.ContextMenuInfo, view: View ) =
+    listsView.selectedContextMenuItem( menuInfo ).asInstanceOf[ TodoList ]
+
+  // Running UI commands
+
+  def doAdd = {
+    val str = findView( TR.newListName ).getText.toString
+    if ( str != "" ) {
+      TodoLists.addList( name = str )
+      findView( TR.newListName ).setText("")
+    }
+  }
+
+  def doRename( list: TodoList ) =
+    renameDialog.doEdit( list.name ){ 
+      TodoLists.setListName( list, _ )
+    }
+
+  def doDelete( list: TodoList ) = {
+    TodoLists.removeList( list )
+    toast( R.string.list_deleted, Toast.LENGTH_LONG )
+  }
+
+  def doUndelete = { 
+    if (TodoLists.numDeletedLists.value > 0) TodoLists.undelete
+    else toast( R.string.undeletes_exhausted )
+  }
+
+  def viewListAt( posn: Int ) {
+    val intent = new Intent( this, classOf[TodoActivity] )
+    val theList = listsView.getAdapter.getItem( posn ).asInstanceOf[ TodoList ]
+    TodoList.intoIntent( theList, intent )
+    startActivity( intent )
+  }
+}
+
+// The activity's generic support dialog (used by the other activity as well).
+
+class EditStringDialog( base: PositronicActivity )
+ extends PositronicDialog( base, layoutResourceId = R.layout.dialog ) 
+ with ViewFinder 
+{
+  val editTxt = findView( TR.dialogEditText )
+  var saveHandler: ( String => Unit ) = null
+
+  editTxt.onKey( KeyEvent.KEYCODE_ENTER ){ doSave; dismiss }
+
+  findView( TR.cancelButton ).onClick { dismiss }
+  findView( TR.saveButton ).onClick { doSave; dismiss }
+  
+  def doSave = saveHandler( editTxt.getText.toString )
+
+  def doEdit( str: String )( handler: String => Unit ) = { 
+    editTxt.setText( str )
+    saveHandler = handler
+    show
+  }
+}
+
+// And now, the other activity, which manages an individual todo list's items.
+
+class TodoActivity 
+ extends PositronicActivity( layoutResourceId = R.layout.todo_one_list ) 
+ with ViewFinder 
+{
+  var theList: TodoList = null
+  var showingDoneItems = true
+
+  lazy val newItemText = findView( TR.newItemText )
+  lazy val listItemsQuery = theList.itemsQuery( showingDoneItems )
+  lazy val listItemsView = findView( TR.listItemsView )
+  lazy val editDialog = new EditStringDialog( this )
+
+  onCreate{
+
+    useOptionsMenuResource( R.menu.items_view_menu )
+    useContextMenuResource( R.menu.item_context_menu )
+
+    // Setup --- get list out of our Intent, and hook up the listItemsView
+
+    theList = TodoList.fromIntent( getIntent )
+    setTitle( "Todo for: " + theList.name )
+
+    useAppFacility( TodoDb )
+    listItemsView.setAdapter( new TodoItemsAdapter( this, listItemsQuery ) )
+
+    // Event handlers...
+
+    listItemsView.onItemClick {( view, posn, id ) =>
+      toggleDone( getDisplayedItem( posn )) }
+
+    findView( TR.addButton ).onClick { doAdd }
+    newItemText.onKey( KeyEvent.KEYCODE_ENTER ){ doAdd }
+
+    onOptionsItemSelected( R.id.delete_where_done ) { deleteWhereDone }
+    onOptionsItemSelected( R.id.undelete ) { undelete }
+    onOptionsItemSelected( R.id.hide_done ) { setShowingDoneItems( false ) }
+    onOptionsItemSelected( R.id.show_done ) { setShowingDoneItems( true ) }
+
+    registerForContextMenu( listItemsView )
+
+    onContextItemSelected( R.id.edit ){ 
+      (menuInfo, view) => doEdit( getContextItem( menuInfo, view ))
+    }
+    onContextItemSelected( R.id.toggledone ){ 
+      (menuInfo, view) => toggleDone( getContextItem( menuInfo, view ))
+    }
+  }
+
+  // UI instance state, less the "super.foo()" noise.
+  // recreateInstanceState runs onCreate, *before* the onCreate handlers.
+
+  override def saveInstanceState( b: Bundle ) = 
+    b.putBoolean( "showing_done_items", showingDoneItems )
+
+  override def restoreInstanceState( b: Bundle ) = 
+    setShowingDoneItems( b.getBoolean( "showing_done_items" ) )
+
+  override def recreateInstanceState( b: Bundle ) = 
+    setShowingDoneItems( b.getBoolean( "showing_done_items" ) )
+
+  // Dealing with mode switching
+
+  def setShowingDoneItems( newValue: Boolean ) = {
+    showingDoneItems = newValue
+    listItemsQuery.requery( showingDoneItems )
+  }
+
+  onPrepareOptionsMenu { menu =>
+    // Hide mode-switch item which switches to the current mode...
+    menu.findItem( R.id.hide_done ).setVisible(  showingDoneItems )
+    menu.findItem( R.id.show_done ).setVisible( !showingDoneItems )
+  }
+
+  // Finding target items for listItemsView taps (including the ContextMenu)
+
+  def getContextItem( menuInfo: ContextMenu.ContextMenuInfo, view: View ) =
+    listItemsView.selectedContextMenuItem( menuInfo ).asInstanceOf[ TodoItem ]
+
+  def getDisplayedItem( posn: Int ) = 
+    listItemsView.getAdapter.getItem( posn ).asInstanceOf[ TodoItem ]
+
+  // Running UI commands
+
+  def doAdd = {
+    val str = newItemText.getText.toString
+    if ( str != "" ) {
+      theList.addItem( description = str, isDone = false )
+      newItemText.setText("")
+    }
+  }
+
+  def doEdit( it: TodoItem ) = 
+    editDialog.doEdit( it.description ) {
+       theList.setItemDescription( it, _ )
+    }
+
+  def toggleDone( it: TodoItem ) = theList.setItemDone( it, !it.isDone )
+
+  def deleteWhereDone = {
+    if (theList.numDoneItems.value > 0) 
+      theList.deleteWhereDone
+    else
+      toast( R.string.no_tasks_done )
+  }
+
+  def undelete = {
+    if (theList.numDeletedItems.value > 0)
+      theList.undeleteItems
+    else
+      toast( R.string.undeletes_exhausted )
+  }
+}
+
+class TodoItemsAdapter( activity: PositronicActivity, 
+                        query: ChangeNotifications[PositronicCursor] )
+ extends CursorSourceAdapter( activity,
+                              source = query,
+                              converter = TodoItem.fromCursor(_),
+                              itemViewResourceId = R.layout.todo_row )
+{
+  def bindItem( view: View, it: TodoItem ) =
+    view.asInstanceOf[ TodoItemView ].setTodoItem( it )
+}
+
+// View for TodoItems:  TextView which adds strikethrough if the item "isDone" 
+
+class TodoItemView( context: Context, attrs: AttributeSet = null )
+ extends TextView( context, attrs ) 
+{
+   var theItem: TodoItem = null
+   def getTodoItem = theItem
+
+   def setTodoItem( item: TodoItem ) = {
+     theItem = item
+     setText( item.description )
+     setPaintFlags( 
+       if (item.isDone) getPaintFlags | Paint.STRIKE_THRU_TEXT_FLAG 
+       else getPaintFlags & ~Paint.STRIKE_THRU_TEXT_FLAG
+     )
+   }
+}
+
+// Getting sub-widgets, using the typed resources consed up by the
+// android SBT plugin.  It would be nice to put this in a library,
+// but the sbt-android plugin puts TypedResource itself in the app's
+// main package --- so the library would have to import it from a
+// different package in every app!
+
+trait ViewFinder {
+  def findView[T](  tr: TypedResource[T] ) = 
+    findViewById( tr.id ).asInstanceOf[T]
+
+  def findViewById( id: Int ): android.view.View
+}
+
